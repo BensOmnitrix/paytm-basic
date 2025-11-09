2654a822b930cdc430fef8b98447335c3260ba92
Lakshay Batra <lakshaynandu.batraben10@gmail.com>
2025-11-09 23:58:04 +0530

    Backend Checkpoint

--- backend/src/controllers/usersInfo.ts
+++ backend/src/controllers/usersInfo.ts
@@ -3,28 +3,37 @@ import { User } from "../db.js";
 import { HTTPStatusCode } from "../statusCodes.js";
 
 export const usersInfo = async (req: Request, res: Response) => {
+  try {
     const _id = (req as any)._id;
-    const filter = req.params.filter as string || "";
+    const filter = (req.query.filter as string);
 
-    if(!filter){
-        return res.status(HTTPStatusCode.BAD_REQUEST).json({
-            message: "Filter parameter is missing"
-        })
+    if (!filter) {
+      return res.status(HTTPStatusCode.BAD_REQUEST).json({
+        message: "Filter parameter is missing",
+      });
     }
-    
+
     const filteredUsers = await User.find({
-        $or: [
-            {firstName: {$regex: filter, $options: "i"}},
-            {lastName: {$regex: filter, $options: "i"}},
-        ]
-    })
+      $or: [
+        { firstName: { $regex: filter, $options: "i" } },
+        { lastName: { $regex: filter, $options: "i" } },
+      ],
+    });
 
     return res.status(HTTPStatusCode.OK).json({
-        users: filteredUsers.map(({email, firstName, lastName, _id}) => {
-            return {
-                email, firstName, lastName, _id
-            }
-        }),
-        message: "Users are filtered successfully"
-    })
-}
+      users: filteredUsers.map(({ email, firstName, lastName, _id }) => {
+        return {
+          email,
+          firstName,
+          lastName,
+          _id,
+        };
+      }),
+      message: "Users are filtered successfully",
+    });
+  } catch (err) {
+    return res.status(HTTPStatusCode.BAD_REQUEST).json({
+      message: (err as any).message,
+    });
+  }
+};
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,44 +1,57 @@
-import mongoose, { Document, Schema } from "mongoose";
+  import mongoose, { Document, Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
+  export interface IUser extends Document {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
   }
-});
 
-export const User = mongoose.model("User", userSchema);
+  const userSchema: Schema<IUser> = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    }
+  });
 
-const accountSchema = new mongoose.Schema({
-  userId : {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-    default: 0,
-    min: 0,
-  },
-});
+  export const User = mongoose.model<IUser>("User", userSchema);
 
-export const Account = mongoose.model("Account", accountSchema);
+  export interface IAccount extends Document {
+    userId : IUser["_id"];
+    amount: number;
+  }
+
+  const accountSchema: Schema<IAccount> = new mongoose.Schema({
+    userId : {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+  });
+
+  export const Account = mongoose.model<IAccount>("Account", accountSchema);
